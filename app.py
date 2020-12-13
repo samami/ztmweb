decdc304c881ff3033a66458113bd6d47d4cca49
Sam Valenti <sam@arts-people.com>
2020-12-13 05:42:26 -1000

    Move data functions to module & make relative url

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, send_from_directory, request
-from pymongo import MongoClient
-from config import mongo_connect_string
+from flask import Flask, render_template, request, url_for
+import dataio
 
 app = Flask(__name__)
 
@@ -9,54 +8,30 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 @app.route('/addhistory')
 def add_history():
     return render_template('add_history.html')
-
-def write_to_file(data):
-    with open('database.txt', mode='a') as database:
-        email = data['email']
-        name = data['name']
-        message = data['message']
-        file = database.write(f'\n{email}, {name}, {message}')
-
-
-def connect_to_mongo():
-    uri = mongo_connect_string
-    client = MongoClient(uri)
-    db = client.general
-    return db
-
-
-def add_data(collection_name, data):
-    db = connect_to_mongo()
-    collection = db[collection_name].insert_one(data)
-
-
-def get_data(collection_name):
-    db = connect_to_mongo()
-    data = db[collection_name].find()
-    return data
 
 
 @app.route('/submit_history', methods=['POST', 'GET'])
 def submit_history():
     if request.method == 'POST':
         data = request.form.to_dict()
-        add_data('work_history',data)
-    return '<a href=http://ztmweb.samami.com/showhistory>Show History</a>'
+        dataio.add_data('work_history',data)
+    return '<a href=' + url_for('show_history') + '>Show History</a>'
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
-        write_to_mongo(data)
+        dataio.add_data('first_collection', data)
     return 'Form Submitted'
 
 
 @app.route('/showhistory')
 def show_history():
-    data = list(get_data('work_history'))
+    data = list(dataio.get_data('work_history'))
     return render_template('show_history.html', data = data)
 
 
